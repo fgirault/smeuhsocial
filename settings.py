0cbeae05eb3d8a0327ac9b3962180f991d810c86
Marandon Alex <al@smeuh.org>
2010-11-10 16:38:23 +0100

    Add initial project files

---
+++ settings.py
@@ -0,0 +1,328 @@
+# -*- coding: utf-8 -*-
+# Django settings for social pinax project.
+
+import os.path
+import posixpath
+import pinax
+import logging
+
+PINAX_ROOT = os.path.abspath(os.path.dirname(pinax.__file__))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+# tells Pinax to use the default theme
+PINAX_THEME = "default"
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# tells Pinax to serve media through the staticfiles app.
+SERVE_MEDIA = DEBUG
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+ADMINS = [
+    # ("Your Name", "your_email@domain.com"),
+]
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "/home/al/smeuhsocial_env/smeuhsocial/db/dev.db",                       # Or path to database file if using sqlite3.
+        "USER": "",                             # Not used with sqlite3.
+        "PASSWORD": "",                         # Not used with sqlite3.
+        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
+        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = "US/Eastern"
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = "en-us"
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = "/pinax/site_media/"
+
+# Absolute path to the directory that holds static files like app media.
+# Example: "/home/media/media.lawrence.com/apps/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
+
+# URL that handles the static files like app media.
+# Example: "http://media.lawrence.com"
+STATIC_URL = "/pinax/"
+
+# Additional directories which hold static files
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, "media"),
+    os.path.join(PINAX_ROOT, "media", PINAX_THEME),
+]
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = "kv9h%g8p8)wv3l(7zcl6z__-i^1#s+o_byutshn%_i+a8aa*h6"
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = [
+    "django.template.loaders.filesystem.load_template_source",
+    "django.template.loaders.app_directories.load_template_source",
+]
+
+MIDDLEWARE_CLASSES = [
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django_openid.consumer.SessionConsumer",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "groups.middleware.GroupAwareMiddleware",
+    "pinax.apps.account.middleware.LocaleMiddleware",
+    "django.middleware.doc.XViewMiddleware",
+    "pagination.middleware.PaginationMiddleware",
+    "django_sorting.middleware.SortingMiddleware",
+    "pinax.middleware.security.HideSensistiveFieldsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+ROOT_URLCONF = "smeuhsocial.urls"
+
+TEMPLATE_DIRS = [
+    os.path.join(PROJECT_ROOT, "templates"),
+    os.path.join(PINAX_ROOT, "templates", PINAX_THEME),
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+    
+    "staticfiles.context_processors.static_url",
+    
+    "pinax.core.context_processors.pinax_settings",
+    
+    "pinax.apps.account.context_processors.account",
+    
+    "notification.context_processors.notification",
+    "announcements.context_processors.site_wide_announcements",
+    "messages.context_processors.inbox",
+    "friends_app.context_processors.invitations",
+    
+    "smeuhsocial.context_processors.combined_inbox_count",
+]
+
+COMBINED_INBOX_COUNT_SOURCES = [
+    "messages.context_processors.inbox",
+    "friends_app.context_processors.invitations",
+    "notification.context_processors.notification",
+]
+
+INSTALLED_APPS = [
+    # Django
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.humanize",
+    "django.contrib.markup",
+    
+    "pinax.templatetags",
+    
+    # external
+    "notification", # must be first
+    "staticfiles",
+    "debug_toolbar",
+    "mailer",
+    "uni_form",
+    "django_openid",
+    "ajax_validation",
+    "timezones",
+    "emailconfirmation",
+    "announcements",
+    "pagination",
+    "friends",
+    "messages",
+    "oembed",
+    "groups",
+    "threadedcomments",
+    "wakawaka",
+    "swaps",
+    "voting",
+    "tagging",
+    "bookmarks",
+    "photologue",
+    "avatar",
+    "flag",
+    "microblogging",
+    "locations",
+    "django_sorting",
+    "django_markup",
+    "tagging_ext",
+    
+    # Pinax
+    "pinax.apps.account",
+    "pinax.apps.signup_codes",
+    "pinax.apps.analytics",
+    "pinax.apps.profiles",
+    "pinax.apps.blog",
+    "pinax.apps.tribes",
+    "pinax.apps.photos",
+    "pinax.apps.topics",
+    "pinax.apps.threadedcomments_extras",
+    "pinax.apps.voting_extras",
+    
+    # project
+    "about",
+]
+
+FIXTURE_DIRS = [
+    os.path.join(PROJECT_ROOT, "fixtures"),
+]
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+ABSOLUTE_URL_OVERRIDES = {
+    "auth.user": lambda o: "/profiles/profile/%s/" % o.username,
+}
+
+MARKUP_FILTER_FALLBACK = "none"
+MARKUP_CHOICES = [
+    ("restructuredtext", u"reStructuredText"),
+    ("textile", u"Textile"),
+    ("markdown", u"Markdown"),
+    ("creole", u"Creole"),
+]
+
+AUTH_PROFILE_MODULE = "profiles.Profile"
+NOTIFICATION_LANGUAGE_MODULE = "account.Account"
+
+ACCOUNT_OPEN_SIGNUP = True
+ACCOUNT_REQUIRED_EMAIL = False
+ACCOUNT_EMAIL_VERIFICATION = False
+ACCOUNT_EMAIL_AUTHENTICATION = False
+ACCOUNT_UNIQUE_EMAIL = EMAIL_CONFIRMATION_UNIQUE_EMAIL = False
+
+AUTHENTICATION_BACKENDS = [
+    "django_auth_ldap.backend.LDAPBackend",
+    "pinax.apps.account.auth_backends.AuthenticationBackend",
+]
+
+LOGIN_URL = "/account/login/" # @@@ any way this can be a url name?
+LOGIN_REDIRECT_URLNAME = "what_next"
+
+EMAIL_CONFIRMATION_DAYS = 2
+EMAIL_DEBUG = DEBUG
+
+ugettext = lambda s: s
+LANGUAGES = [
+    ("en", u"English"),
+]
+
+# URCHIN_ID = "ua-..."
+
+YAHOO_MAPS_API_KEY = "..."
+
+class NullStream(object):
+    def write(*args, **kwargs):
+        pass
+    writeline = write
+    writelines = write
+
+RESTRUCTUREDTEXT_FILTER_SETTINGS = {
+    "cloak_email_addresses": True,
+    "file_insertion_enabled": False,
+    "raw_enabled": False,
+    "warning_stream": NullStream(),
+    "strip_comments": True,
+}
+
+# if Django is running behind a proxy, we need to do things like use
+# HTTP_X_FORWARDED_FOR instead of REMOTE_ADDR. This setting is used
+# to inform apps of this fact
+BEHIND_PROXY = False
+
+FORCE_LOWERCASE_TAGS = True
+
+# Uncomment this line after signing up for a Yahoo Maps API key at the
+# following URL: https://developer.yahoo.com/wsregapp/
+# YAHOO_MAPS_API_KEY = ""
+
+DEBUG_TOOLBAR_CONFIG = {
+    "INTERCEPT_REDIRECTS": False,
+}
+
+
+import ldap
+from django_auth_ldap.config import LDAPSearch, PosixGroupType
+
+AUTH_LDAP_BIND_DN = ""
+AUTH_LDAP_BIND_PASSWORD = ""
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=people,o=loc",
+    ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+
+# This doesn't work very well
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail"
+}
+
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=people,o=loc",
+    ldap.SCOPE_SUBTREE, "(objectClass=posixGroup)"
+)
+
+AUTH_LDAP_GROUP_TYPE = PosixGroupType()
+
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    "is_active": "cn=admin,ou=groups,o=loc",
+    "is_staff": "cn=admin,ou=groups,o=loc",
+    "is_superuser": "cn=admin,ou=groups,o=loc"
+}
+
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+)
+
+ldap_log = logging.getLogger('django_auth_ldap')
+ldap_log_handler = logging.StreamHandler()
+ldap_log_handler.setLevel(logging.DEBUG)
+ldap_log.addHandler(ldap_log_handler)
+
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
+
