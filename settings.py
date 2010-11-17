f6a284121c0585e1cad6d4ebc9857fa537527d9b
Alex Marandon <contact@alexmarandon.com>
2010-11-17 22:21:25 +0700

    Fix hardcoded path to db file

--- settings.py
+++ settings.py
@@ -31,7 +31,7 @@ MANAGERS = ADMINS
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "/home/al/smeuhsocial_env/smeuhsocial/db/dev.db",                       # Or path to database file if using sqlite3.
+        "NAME": os.path.join(PROJECT_ROOT, "db", "dev.db"),                       # Or path to database file if using sqlite3.
         "USER": "",                             # Not used with sqlite3.
         "PASSWORD": "",                         # Not used with sqlite3.
         "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
