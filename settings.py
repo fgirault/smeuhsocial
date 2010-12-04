585c9159e78dab642e7b9f5bf4fcec27fba958c8
smeuhsocial <smeuhsocial@smeuh.org>
2010-12-04 04:42:23 +0100

    Add middleware to send exceptions to Apache log

--- settings.py
+++ settings.py
@@ -107,6 +107,7 @@ MIDDLEWARE_CLASSES = [
     "django_sorting.middleware.SortingMiddleware",
     "pinax.middleware.security.HideSensistiveFieldsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "smeuhsocial.middleware.WsgiLogErrors",
 ]
 
 ROOT_URLCONF = "smeuhsocial.urls"
