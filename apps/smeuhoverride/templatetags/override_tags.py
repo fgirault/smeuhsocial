a057f49cb8311579f405d622059e11953bb6fb09
Alex Marandon <contact@alexmarandon.com>
2011-06-08 00:52:39 +0700

    Make @username link to profile page in comments

--- apps/smeuhoverride/templatetags/override_tags.py
+++ apps/smeuhoverride/templatetags/override_tags.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from microblogging.templatetags import microblogging_tags
 from django import template
+from django.utils.safestring import mark_safe
+from microblogging.templatetags.microblogging_tags import (
+        render_tweet_text as _render_tweet_text,
+        user_ref_re,
+        make_user_link,
+        )
 
 register = template.Library()
 
@@ -11,10 +16,18 @@ def render_tweet_text(tweet):
     """
     Override render_tweet_text from microblogging to make URLs clickable
     """
-    text = microblogging_tags.render_tweet_text(tweet)
+    text = _render_tweet_text(tweet)
     text = template.defaultfilters.urlize(text)
     return text
 
 @register.inclusion_tag("_user_link.html")
 def profile_link(member):
     return {'member': member}
+
+@register.filter
+def profilize(text):
+    """
+    Replace @username by a link to the profile page
+    """
+    text = user_ref_re.sub(make_user_link, text)
+    return mark_safe(text)
