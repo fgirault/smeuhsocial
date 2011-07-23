5dcba5d842209791918ea67ea95895a23475a803
Alex Marandon <contact@alexmarandon.com>
2011-07-24 00:32:06 +0700

    Delete tags for deleted items

--- apps/smeuhoverride/models.py
+++ apps/smeuhoverride/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import signals
+from tagging.models import TaggedItem
 
-# Create your models here.
+def taggeditem_delete(sender, **kwargs):
+    """
+    Deletes TaggedItems for ALL deleted model instances
+    Workaround for bug:
+        http://code.google.com/p/django-tagging/issues/detail?id=162
+    """
+    deleted = kwargs['instance']
+    try:
+        id = int(deleted.pk)
+    except ValueError:
+        return
+    ctype = ContentType.objects.get_for_model(deleted)
+    item_tags = TaggedItem.objects.filter(
+            content_type=ctype,
+            object_id=id,
+        )
+    item_tags.delete()
+
+signals.post_delete.connect(taggeditem_delete)
