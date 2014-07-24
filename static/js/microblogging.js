ad1a3be3e2c79b6c15d33fdb833e269df184f32b
François Girault <fgirault@gmail.com>
2014-07-24 23:31:18 +0200

    touite form tip about number of chars refactored to a bootstrap popover when focus hits the text area

--- static/js/microblogging.js
+++ static/js/microblogging.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
     if ($('#new_tweet').length) {
       function update_chars_left() {
+    	  $('#new_tweet').popover('show');
           var max_len = 140;
           var textarea = $('#new_tweet')[0];
           var tweet_len = textarea.value.length;
@@ -8,14 +9,13 @@ jQuery(document).ready(function($) {
               textarea.value = textarea.value.substring(0, max_len); // truncate
               $('#chars_left').html("0");
           } else {
-              $('#chars_left').html(max_len - tweet_len);
+        	  $('#chars_left').html(max_len - tweet_len);
           }
       }
+      
       $('#new_tweet').keyup(function() {
           update_chars_left();
       });
-      update_chars_left();
-      $('#new_tweet').focus();
-    }
-});
 
+    };
+});
