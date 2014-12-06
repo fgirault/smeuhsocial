5817d1ecb2afdafa5824577ee4f908687c9071bb
François Girault <fgirault@gmail.com>
2014-12-06 13:03:51 +0100

    add audio artist view

--- apps/artist/urls.py
+++ apps/artist/urls.py
@@ -3,5 +3,6 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns("",
     # all items
-    url(r"^$", "artist.views.index", name="artist_index"),   
+    url(r"^$", "artist.views.index", name="artist_index"),
+    url(r"^(?P<name>.+)/$", "artist.views.artist", name="artist_tracks"),   
 )
--- apps/artist/views.py
+++ apps/artist/views.py
@@ -37,3 +37,9 @@ def index(request):
     
     
     return render_to_response("artist/index.html", { 'artists': names, 'columns': columns }, context_instance = RequestContext(request))
+
+def artist(request, name):
+    
+    tracks = get_track_model().objects.filter(artist = name).order_by('-created_at')
+    
+    return render_to_response("artist/artist.html", {'tracks': tracks, 'artist': name }, context_instance = RequestContext(request))
